dc546801f50f4d2c4fe8c4a40b14573163e54da9
Norbert Schulze <NorbertHD@users.noreply.github.com>
2025-05-24 19:49:08 +0200

    Using `climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA` is deprecated

--- components/acp24/climate.py
+++ components/acp24/climate.py
@@ -9,16 +9,14 @@ AUTO_LOAD = ["climate_ir"]
 acp24_ns = cg.esphome_ns.namespace("acp24")
 Acp24Climate = acp24_ns.class_("Acp24Climate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+CONFIG_SCHEMA = climate_ir.climare_ir_with_receiver_schema(Acp24Climate).extend(
     {
-        cv.GenerateID(): cv.declare_id(Acp24Climate),
         cv.GenerateID(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
     }
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await climate_ir.new_climate_ir(config)
     time_ = await cg.get_variable(config[CONF_TIME_ID])
     cg.add(var.set_time(time_))
     cg.add(var.set_supports_heat(False))
-    await climate_ir.register_climate_ir(var, config)
